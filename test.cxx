60e03113aca4d82dee24b3b3547ea453fcf7b858
d3suu <mateusz.dukat1@gmail.com>
2022-01-01 23:20:21 +0000

    Initial

---
+++ test.cxx
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "test.hxx"
+
+int main(){
+	std::cout << "Test " << VER << " at " << KERNEL_VER << std::endl;
+	return 0;
+}
+
---
+++ test.hxx
@@ -0,0 +1,2 @@
+#define VER "1.0"
+#define KERNEL_VER "5.11.0-1022-oracle"
