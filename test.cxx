aa2a4d7484d07fa93b0c0cb92df4c9621e0ea27e
d3suu <mateusz.dukat1@gmail.com>
2022-01-02 00:37:55 +0000

    Pistache CMake

--- test.cxx
+++ test.cxx
@@ -1,8 +1,26 @@
 #include <iostream>
+#include <pistache/endpoint.h>
 #include "test.hxx"
+
+using namespace Pistache;
+
+class HelloHandler : public Http::Handler {
+	public:
+		HTTP_PROTOTYPE(HelloHandler);
+		void onRequest(const Http::Request& request, Http::ResponseWriter response) {
+			response.send(Http::Code::Ok, "Hello, world\n");
+		}
+};
 
 int main(){
 	std::cout << "Test " << VER << " at " << KERNEL_VER << std::endl;
+	Address addr(Ipv4::any(), Port(8080));
+
+	auto opts = Http::Endpoint::options().threads(1);
+	Http::Endpoint server(addr);
+	server.init(opts);
+	server.setHandler(Http::make_handler<HelloHandler>());
+	server.serve();
 	return 0;
 }
 
